542ca4ff9938df9742b73b0219837ff8a633736a
Refat Baderkhan <refat@192.168.1.4>
2025-08-07 22:05:34 +0300

    working everywhere not mobile

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,13 @@ import PSketch from "./PSketch";
 
 function App() {
   return (
-    <div
-      style={{
-        backgroundColor: "",
-        height: "100vh",
-        width: "100vw",
-        margin: 0,
-        padding: 0,
-        overflow: "hidden",
-      }}
-    >
-      <PSketch SVG={FinalSVG} />
+    <div className="app">
+      <div id="svg-wrapper">
+        <div id="svg-container">
+          <FinalSVG />
+          <PSketch />
+        </div>
+      </div>
     </div>
   );
 }
