fcc7f9ced733d37b681c0f9f6ac4364b1b8f1361
Refat Baderkhan <refat@192.168.1.4>
2025-08-07 19:26:33 +0300

    before p5js: working

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import "./App.css";
 import React from "react";
-import GlowingSVG from "./GlowingSVG";
-import parseSVG from "./parseSVG";
+// import GlowingSVG from "./GlowingSVG";
+// import parseSVG from "./parseSVG";
 import FinalSVG from "./Final_SVG";
+import Sketch from "./PSketch";
 
 function App() {
   return (
     <div
       style={{
-        backgroundColor: "#fadadd",
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        paddingLeft: "2rem",
+        backgroundColor: "", // Fixed: removed quotes around black
+        height: "100vh",
+        width: "100vw", // Changed from 100vh to 100vw for full width
+        margin: 0, // Remove default margins
+        padding: 0, // Remove default padding
+        overflowY: "hidden",
+        overflowX: "hidden",
+        position: "relative", // Added for proper positioning context
       }}
     >
-      <div>
+      <div
+        style={{
+          position: "absolute", // Changed from fixed to absolute
+          bottom: 0,
+          left: 0,
+          width: "auto", // Changed from 100% to auto for SVG natural width
+          height: "auto", // Changed to auto for SVG natural height
+        }}
+      >
         <FinalSVG />
       </div>
     </div>
